aa3715fb9e83301a7228b59134d8b02c78fde2c0
Luka <luka.macieszczak@gmail.com>
2023-02-10 22:17:04 -0500

    Bob

--- hackathon/src/App.js
+++ hackathon/src/App.js
@@ -9,13 +9,15 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Bob
         </a>
       </header>
     </div>
